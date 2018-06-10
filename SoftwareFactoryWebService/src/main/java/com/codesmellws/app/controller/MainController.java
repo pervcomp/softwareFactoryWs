b8e3e4bb1273ae94467cb44919171f25358d8763
luca9294 <luca9294@hotmail.it>
2018-06-10 16:33:11 +0200

    main

--- SoftwareFactoryWebService/src/main/java/com/codesmellws/app/controller/MainController.java
+++ SoftwareFactoryWebService/src/main/java/com/codesmellws/app/controller/MainController.java
@@ -26,7 +26,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.kotlin.App;
+import com.kotlin.ScanOptionsKt;
 
  
 import com.mongodb.BasicDBObject;
